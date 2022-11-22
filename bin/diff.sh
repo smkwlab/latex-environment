1e7b5bb515034f75b5a48ba97c4c75e1e7938e8c
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2022-11-22 13:48:24 +0900

    diff 用スクリプトを追加

---
+++ bin/diff.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global --add safe.directory /workdir
+# 一つ前のコミットとこのコミットの差分
+latexdiff-vc -e utf8 -t CFONT --git --flatten --force -r HEAD main.tex
+
+# 現在と指定したIDの差分
+# latexdiff-vc -e utf8 -t CFONT --git --flatten --force -r commit-ID main.tex
+
+# 一つ前のタグと最新のタグの差分
+# git tag | sort -V | tail -n 2 | xargs -n 2 bash -c 'latexdiff-vc -e utf8 -t CFONT --git --flatten --force -r $0 -r $1 -t CFONT main.tex'
