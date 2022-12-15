0f1c6d1ebe7bb1d85a96497e3e7867ebf46ee5da
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2022-12-15 14:50:11 +0900

    add .gitignore update

--- bin/setup-sotsuron.sh
+++ bin/setup-sotsuron.sh
@@ -2,5 +2,7 @@
 git remote add sotsuron https://github.com/smkwlab/sotsuron-template.git
 git fetch sotsuron
 git merge --allow-unrelated-histories sotsuron/main
+cat .gitignore-sotsuron >> .gitignore
+rm .gitignore-sotsuron
 git commit -m 'sotsuron repository initialized.'
 git push
